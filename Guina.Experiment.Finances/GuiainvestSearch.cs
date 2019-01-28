cfd50936ca0c3f82424e187dad5875a251574603
Agnaldo Costa de Almeida <agnaldo.almeida@iteris.com.br>
2019-01-28 20:11:47 -0200

    Add project files.

---
+++ Guina.Experiment.Finances/GuiainvestSearch.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guina.Experiment.Finances
+{
+    public static class GuiainvestSearch
+    {
+        const String GUIAINVEST_QUERY = "https://www.guiainvest.com.br/mural/";
+
+        public static String GetTickerPrice(String ticker)
+        {
+            try
+            {
+                HttpWebRequest webRequest = (HttpWebRequest)HttpWebRequest.Create(String.Concat(GUIAINVEST_QUERY, ticker, ".aspx"));
+
+                webRequest.ContentType = "text/html";
+                webRequest.Method = "GET";
+
+                using (var response = (HttpWebResponse)webRequest.GetResponse())
+                {
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        var stream = new StreamReader(response.GetResponseStream()).ReadToEnd();
+                        int index = stream.IndexOf("osci_" + ticker.ToUpper());
+                        string conteudo = stream.Substring(index, 50);
+
+                        int start = conteudo.IndexOf(">");
+                        int end = conteudo.IndexOf("%");
+                        string sinal = "+";
+
+                        if (conteudo.IndexOf("changeup") > 0)
+                        {
+                            start = conteudo.IndexOf(";");
+                            sinal = "+";
+                        }
+                        else if (conteudo.IndexOf("changedown") > 0)
+                        {
+                            start = conteudo.IndexOf(";");
+                            sinal = "-";
+                        }
+
+                        return String.Concat(sinal, conteudo.Substring(start + 1, end - start));
+                    }
+                    else
+                        return "Nada encontrado";
+                }
+            }
+            catch (WebException ex)
+            {
+                var stream = new StreamReader(ex.Response.GetResponseStream()).ReadToEnd();
+                return stream;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="tickers"></param>
+        /// <returns></returns>
+        public static void GetTickersPrices(params String[] tickers)
+        {
+            foreach (string ticker in tickers)
+            {
+                Console.WriteLine(String.Concat(ticker, ":", GetTickerPrice(ticker)));
+            }
+        }
+    }
+}
---
+++ Guina.Experiment.Finances/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guina.Experiment.Finances
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("ABEV3:");
+            //Console.WriteLine(GuiainvestSearch.GetTickerPrice("ABEV3"));
+            //Console.WriteLine("BBAS3:");
+            //Console.WriteLine(GuiainvestSearch.GetTickerPrice("BBAS3"));
+
+            GuiainvestSearch.GetTickersPrices(ConfigurationManager.AppSettings["tickers"].Split('|'));
+
+            Console.ReadLine();
+        }
+    }
+}
